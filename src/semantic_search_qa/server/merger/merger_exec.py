67d53caacd1158eabadbcf132dc06d0b89a594b1
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2022-09-26 19:38:03 -0700

    Add classifier model and improve UI
    Manually merge the results of the two models in the merger executor
    
    This aims to close #15

--- src/semantic_search_qa/server/merger/merger_exec.py
+++ src/semantic_search_qa/server/merger/merger_exec.py
@@ -1,4 +1,5 @@
 from jina import Document, DocumentArray, Executor, requests
+from numpy import append
 
 from semantic_search_qa.server.server_utils import log_exec_basics
 
@@ -15,8 +16,25 @@ class MergerExecutor(Executor):
         """
         log_exec_basics(self.metas.name, self.logger, docs, kwargs)
 
+        qa_doc = docs[0]
+        cls_doc = docs[1]
+
+        new_doc = Document(text=qa_doc.text)
+        new_chunks = DocumentArray()  # type: ignore
+        for qa_c, cls_c in zip(qa_doc.chunks, cls_doc.chunks):
+            new_chunk = qa_c
+            new_chunk.tags["sentiment"] = cls_c.tags["sentiment"]
+            new_chunk.scores["cls_score"] = cls_c.scores["cls_score"]
+            new_chunks.append(new_chunk)
+        new_doc.chunks = new_chunks
+
+        new_da = DocumentArray(new_doc)
+        self.logger.info(new_da[0].summary())
+
         # TODO Do this here or in the ranker? Maybe here to give this exec a a purpose...
         limit_result_idx = int(kwargs["parameters"]["n_of_results"])
-        for i, d in enumerate(docs):
+        for i, d in enumerate(new_da):
             self.logger.info(f"Limiting doc {i} from {len(docs[i].chunks)} results to {limit_result_idx}")
             d.chunks = d.chunks[:limit_result_idx]
+
+        return new_da
