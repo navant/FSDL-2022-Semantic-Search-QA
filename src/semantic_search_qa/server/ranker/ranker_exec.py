09b7fc7ed11e26a5f2124104d85dcc1b28ad7fd6
psxz <priyam.sadhukhan@gmail.com>
2022-09-27 17:45:47 -0400

    Added rankerExecutor

---
+++ src/semantic_search_qa/server/ranker/ranker_exec.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict
+
+from docarray.score import NamedScore
+from jina import DocumentArray, Executor, requests
+from transformers.pipelines import pipeline
+
+from semantic_search_qa.server.server_utils import log_exec_basics
+
+param_top_k_ranker = 3
+model = "anablasi/qa_financial_v2"
+
+
+class RankingExecutor(Executor):
+    def __init__(self, device: str = "cpu", *args, **kwargs):
+        """The Ranking executor running on a CPU or a GPU
+
+        :param device: The pytorch device that the model is on, e.g. 'cpu', 'cuda', 'cuda:1'
+        """
+        super().__init__(*args, **kwargs)
+
+    @requests  # (on="/qa")
+    async def add_text(self, docs: DocumentArray, **kwargs):
+        log_exec_basics(self.metas.name, self.logger, docs, kwargs)
+
+        limit_result_idx = int(kwargs["parameters"]["n_of_results"])  # TODO This should be done in the ranker
+        self.logger.info(f"--------------------------- Returning only {limit_result_idx} results")
+
+        for d in docs:
+            d.chunks = sorted(d.chunks, key=lambda x: x["score"], reverse=True)[:limit_result_idx]
+            
