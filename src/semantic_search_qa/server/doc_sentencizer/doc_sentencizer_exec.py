90bddcf5fe4df00fd37ad8ed0de0170efdadfaf6
SamT <samtonetto@gmail.com>
2022-09-27 13:46:03 +1000

    added linting

--- src/semantic_search_qa/server/doc_sentencizer/doc_sentencizer_exec.py
+++ src/semantic_search_qa/server/doc_sentencizer/doc_sentencizer_exec.py
@@ -4,19 +4,19 @@ from semantic_search_qa.server.server_utils import log_exec_basics
 
 
 class DocSentencizerExecutor(Executor):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-
     def clean_text(self, text):
         """
-        Remove newlines and tabs.
+        Basically just remove newlines and tabs.
+
+        Don't want to remove punctuation or case because it can convey sentiment or other contextual information.
         """
         text = text.split()
-        text = ' '.join(text)
-        return text
+        text = " ".join(text)
 
+        return text
 
     @requests(on="/doc_sentencizer")
     async def sentencize_text_chunks(self, docs: DocumentArray, **kwargs):
@@ -29,12 +29,11 @@ class DocSentencizerExecutor(Executor):
 
         for d in docs:
             d.text = self.clean_text(d.text)
-        
+
         sentencizer = Executor.from_hub("jinahub://SpacySentencizer", install_requirements=True)
         sentencizer.segment(docs, parameters={})
 
-        
         self.logger.info(f"number of docs: {len(docs)}")
         for d in docs:
-            self.logger.info(f'Number of chunks after sentencizing:  {len(d.chunks)}.')
-            self.logger.info(f'Sentences: {[c.text for c in d.chunks]}')
+            self.logger.info(f"Number of chunks after sentencizing:  {len(d.chunks)}.")
+            self.logger.info(f"Sentences: {[c.text for c in d.chunks]}")
