5f63251e2113c9766d815a93b89ec06446a4a54a
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2022-09-30 13:14:21 -0700

    Fix workflow topology
    Now it's working again.
    
    Also segregate QA and Classification Outputs in the UI.

--- src/semantic_search_qa/server/doc_chunker/doc_chunker_exec.py
+++ src/semantic_search_qa/server/doc_chunker/doc_chunker_exec.py
@@ -1,3 +1,4 @@
+from importlib_metadata import SelectableGroups
 from jina import Document, DocumentArray, Executor, requests
 
 from semantic_search_qa.server.server_utils import log_exec_basics
@@ -5,10 +6,15 @@ from semantic_search_qa.utils import chunk_text
 
 
 class DocChunkerExecutor(Executor):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, chunk_len: int, do_overlap: bool, overlap_char_len: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.chunk_len = chunk_len
+        self.do_overlap = do_overlap
+        self.overlap_char_len = overlap_char_len
+        self.logger.info(f"Chunk size: {self.chunk_len}")
+        self.logger.info(f"Do Overlap? {self.do_overlap} - Overlap size: {self.overlap_char_len}")
 
-    @requests(on="/doc_chunker")
+    @requests  # (on="/doc_chunker")
     async def chunk_doc_text(self, docs: DocumentArray, **kwargs):
         """
         This processes each of the documents received. Each of the documents contains text, which is going to
@@ -19,17 +25,23 @@ class DocChunkerExecutor(Executor):
         log_exec_basics(self.metas.name, self.logger, docs, kwargs)
 
         try:
-            # TODO: Add chunk_len in to the parameters!
             chunk_len = int(kwargs["parameters"]["chunk_len"])
         except KeyError:
-            self.logger.warning("Couldn't extract chunk_len from Document parameters. Using default of 100")
-            chunk_len = 100
+            chunk_len = self.chunk_len
+            self.logger.warning(f"Couldn't extract chunk_len from Document parameters. Using defaulf of {chunk_len}")
+
+        try:
+            self.overlap_char_len = int(kwargs["parameters"]["overlap_char_len"])
+        except KeyError:
+            self.logger.warning(f"Couldn't extract chunk_len from Document parameters. Using defaulf of {chunk_len}")
 
         for d in docs:
             self.logger.info("\n" + 100 * "=" + "\n" + f"Processing Document {d.id}\n" + 100 * "=")
-            chunks = chunk_text(d.text, chunk_len=chunk_len)
+            chunks = chunk_text(
+                d.text, chunk_len=chunk_len, do_overlap=self.do_overlap, overlap_size=self.overlap_char_len
+            )
             for chunk in chunks:
-                self.logger.info(f"Adding new Document chunk to {d.id}\n" + 100 * "-" + "\n" + f"{chunk}\n")
+                self.logger.debug(f"Adding new Document chunk to {d.id}\n" + 100 * "-" + "\n" + f"{chunk}\n")
                 new_chunk = Document(text=chunk)
                 self.logger.debug(
                     f"(Before adding it as chunk) Doc id {d.id} vs New Doc parent id {new_chunk.parent_id}"
@@ -38,3 +50,5 @@ class DocChunkerExecutor(Executor):
                 self.logger.debug(
                     f"(After adding it as chunk) Doc id {d.id} vs New Doc parent id {new_chunk.parent_id}"
                 )  # Parent id should have been filled!
+
+        self.logger.info(f"Chunks\n{docs.summary()}")
