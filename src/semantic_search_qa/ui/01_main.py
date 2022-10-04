6ad916e1c85a52f1c7427c388b7d892286a7d44d
Navan Tirupathi <navant@gmail.com>
2022-10-04 14:52:15 +1100

    commit

--- src/semantic_search_qa/ui/01_main.py
+++ src/semantic_search_qa/ui/01_main.py
@@ -140,7 +140,7 @@ with st.form("main-form", clear_on_submit=True):
             "backend":client_params["backend"],
             "host": client_params["host"],
             "port": client_params["port"],
-            "endpoint": "/doc_cleaner",  # TODO Improve this entry point to have a better name
+            "endpoint": "/doc_cleaner",
             "n_of_results": n_of_results,
         }
         send_qa_request(**req_args)
